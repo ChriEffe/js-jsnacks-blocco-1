a96b706ed4d844756e2dce7a147480c2b1a3b956
ChriEffe <christianferrazzano93@gmail.com>
2021-11-29 14:27:25 +0100

    while

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,8 +1,17 @@
 /*Il software deve chiedere per 10 volte all’utente di inserire un numero.Il programma stampa la somma di tutti i numeri inseriti.*/
 
 let sum = 0;
-for (let i = 0; i < 10; i++) {
+/*for (let i = 0; i < 10; i++) {
     const number = parseInt(prompt('Inserisci un numero'));
     sum += number;
 }
+console.log(sum);*/
+
+let i = 0;
+while (i < 10) {
+    const number = parseInt(prompt('Inserisci un numero'));
+    sum += number;
+
+    i++
+}    
 console.log(sum);
